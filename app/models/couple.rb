a8ca0f2bccad0765b2500ea952b940eb5bbc0a29
Elson Otake <elsonotake@yahoo.com.br>
2023-05-06 00:33:33 -0300

    Create couple model

---
+++ app/models/couple.rb
@@ -0,0 +1,3 @@
+class Couple < ApplicationRecord
+  has_and_belongs_to_many :trees
+end
---
+++ test/system/couples_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class CouplesTest < ApplicationSystemTestCase
+  setup do
+    @couple = couples(:one)
+  end
+
+  test "visiting the index" do
+    visit couples_url
+    assert_selector "h1", text: "Couples"
+  end
+
+  test "should create couple" do
+    visit couples_url
+    click_on "New couple"
+
+    fill_in "Local", with: @couple.local
+    fill_in "Marriage", with: @couple.marriage
+    fill_in "Separation", with: @couple.separation
+    fill_in "Tree1", with: @couple.tree1_id
+    fill_in "Tree2", with: @couple.tree2_id
+    click_on "Create Couple"
+
+    assert_text "Couple was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Couple" do
+    visit couple_url(@couple)
+    click_on "Edit this couple", match: :first
+
+    fill_in "Local", with: @couple.local
+    fill_in "Marriage", with: @couple.marriage
+    fill_in "Separation", with: @couple.separation
+    fill_in "Tree1", with: @couple.tree1_id
+    fill_in "Tree2", with: @couple.tree2_id
+    click_on "Update Couple"
+
+    assert_text "Couple was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Couple" do
+    visit couple_url(@couple)
+    click_on "Destroy this couple", match: :first
+
+    assert_text "Couple was successfully destroyed"
+  end
+end
