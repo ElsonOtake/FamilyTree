a947ff55d12d4c514535bf29476d852f031400d0
Elson Otake <elsonotake@yahoo.com.br>
2023-05-14 22:39:03 -0300

    Recreate table with kanji names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_14_131948) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_14_171948) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
@@ -54,6 +54,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_14_131948) do
 
   create_table "people", force: :cascade do |t|
     t.string "name"
+    t.string "kanji"
     t.integer "gender"
     t.boolean "alive", default: true
     t.date "birth"
