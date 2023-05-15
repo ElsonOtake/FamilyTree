943306ce93470c1e4402517b2ad17ae420bb863b
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-14 22:28:45 -0300

    Add new column for kanji names

--- db/migrate/20230514010344_create_people.rb
+++ db/migrate/20230514010344_create_people.rb
@@ -2,6 +2,7 @@ class CreatePeople < ActiveRecord::Migration[7.0]
   def change
     create_table :people do |t|
       t.string :name
+      t.string :kanji
       t.integer :gender
       t.boolean :alive, default: true
       t.date :birth
