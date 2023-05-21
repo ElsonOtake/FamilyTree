f5eb5d60a76b9e6e1356fd1e8bf6583daaa682bf
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-21 13:17:08 -0300

    Update color of the selected button

--- app/javascript/controllers/stimulus_controller.js
+++ app/javascript/controllers/stimulus_controller.js
@@ -2,10 +2,21 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="stimulus"
 export default class extends Controller {
+  initialize() {
+    const buttons = document.querySelectorAll(".first-letter-button");
+    buttons[0].classList.add("is-info");
+  }
+
   burger() {
     const burger = document.querySelector(".navbar-burger");
     const nav = document.querySelector("#"+burger.dataset.target);
     burger.classList.toggle("is-active");
     nav.classList.toggle("is-active");
+  }
+
+  select() {
+    const buttons = document.querySelectorAll(".first-letter-button");
+    buttons.forEach(button => button.classList.remove("is-info"));
+    this.element.classList.toggle("is-info");
   }
 }
