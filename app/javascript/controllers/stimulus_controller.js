78d363af58be0bb3047aef95619c7cb586bd8f19
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-21 15:07:47 -0300

    Update first selected button setup code

--- app/javascript/controllers/stimulus_controller.js
+++ app/javascript/controllers/stimulus_controller.js
@@ -2,11 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="stimulus"
 export default class extends Controller {
-  initialize() {
-    const buttons = document.querySelectorAll(".first-letter-button");
-    buttons[0].classList.add("is-info");
-  }
-
   burger() {
     const burger = document.querySelector(".navbar-burger");
     const nav = document.querySelector("#"+burger.dataset.target);
