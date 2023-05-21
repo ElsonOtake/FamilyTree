f12f4dfa2f9e905641e60726b413491eda3583e7
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-21 11:52:27 -0300

    Remove unused controller

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,7 +4,6 @@
 //= link_person ../../../vendor/javascript .js
 //= link application.js
 //= link controllers/application.js
-//= link controllers/hello_controller.js
 //= link controllers/stimulus_controller.js
 //= link controllers/index.js
 //= link EAO.png
