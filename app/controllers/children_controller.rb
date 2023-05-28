644641f3d4e0e77147c65c7da887cd2315638087
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-28 16:54:03 -0300

    Create list of person without identified parents

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -1,8 +1,11 @@
 class ChildrenController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_couple
   def index
   end
 
   def new
+    @people = Person.where.not(id: Person.find_by_sql("Select person_id from couples_people").pluck(:person_id)).order(:name)
   end
 
   def create
@@ -18,5 +21,11 @@ class ChildrenController < ApplicationController
   end
 
   def show
+  end
+
+  private
+
+  def set_couple
+    @couple = Couple.find(params[:couple_id])
   end
 end
