3fe39103e83ce043c064c513492d1a7bf5bc3052
Elson Otake <elsonotake@yahoo.com.br>
2023-05-28 14:21:59 -0300

    Update couple routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :people do
-    resources :couples
+    resources :couples, only: [:new, :edit]
   end
+  resources :couples, except: [:new, :edit]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
