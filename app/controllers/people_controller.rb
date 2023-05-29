d00ca6e4964451c3f3d9d86e48f02300c3f4ec40
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-05-28 21:25:47 -0300

    Add turbo stream actions to create and update

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -56,10 +56,11 @@ class PeopleController < ApplicationController
     respond_to do |format|
       if @person.save
         format.html { redirect_to person_url(@person), notice: "Person was successfully created." }
-        format.json { render :show, status: :created, location: @person }
+        format.turbo_stream { flash.now[:notice] = "Person was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @person.errors, status: :unprocessable_entity }
+        flash.now[:notice] = @person.errors.full_messages[0]
+        format.turbo_stream { render turbo_stream: helpers.render_turbo_stream_inline_flash_messages }
       end
     end
   end
