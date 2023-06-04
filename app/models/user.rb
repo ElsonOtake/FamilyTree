03ae4188bd415337dd8f5bcd679ae7a18f480eae
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-06-04 18:57:06 -0300

    Change email to downcase before save

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  before_save :to_lowercase
   validates :name, presence: true
+
+  def to_lowercase
+    email.downcase!
+  end
 end
