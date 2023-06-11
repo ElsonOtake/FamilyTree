cf314105f02176e8fa7ada3b3d17591040421cfc
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2023-06-11 19:43:48 -0300

    Add slug column to people table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_04_004851) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_11_224252) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gin"
   enable_extension "btree_gist"
@@ -62,6 +62,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_04_004851) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_people_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
