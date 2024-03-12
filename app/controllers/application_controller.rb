4c7d05b7e1c8af7fd4315353f1e34c292542bade
Elson Otake <elsonotake@yahoo.com.br>
2024-03-12 11:05:17 -0300

    Add route to translate unidentified devise pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   around_action :switch_locale
 
   def switch_locale(&action)
-    locale = current_user.try(:locale) || I18n.default_locale
+    locale = current_user.try(:locale) || session[:locale] || I18n.default_locale
     I18n.with_locale(locale, &action)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       resources :children
     end
     patch 'change/:locale', to: 'people#change', as: 'locale_change'
+    patch 'change_unidentified/:locale', on: :collection, to: 'people#change_unidentified', as: 'locale_change_unidentified'
   end
   resources :couples, only: [:index, :show, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
