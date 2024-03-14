04e2731987d29e77e5df492c8c7ba60567c3976f
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2024-03-13 21:55:12 -0300

    Update age feature

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,3 +1,16 @@
 module PeopleHelper
   include Pagy::Frontend
+
+  def age(from, to)
+    if to > from
+      days = (to - from).to_i
+      if days / 365.25 > 1
+        t('datetime.distance_in_words.x_years_old', count: (days / 365.25).to_i)
+      elsif days / 30.6 > 1
+        t('datetime.distance_in_words.x_months', count: (days / 30.6).to_i)
+      else
+        t('datetime.distance_in_words.x_days', count: days)
+      end
+    end
+  end
 end
