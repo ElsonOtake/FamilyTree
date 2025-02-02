360e17bbb8cb8fb4788b790d1c477b0dcf42f318
Elson Otake <elsonotake@yahoo.com.br>
2025-02-02 16:20:53 -0300

    Create global email address

--- app/mailers/family_mailer.rb
+++ app/mailers/family_mailer.rb
@@ -1,54 +1,39 @@
+# frozen_string_literal: true
+
+# This mailer manages the emails sent to the user.
 class FamilyMailer < ApplicationMailer
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.family_mailer.person_created.subject
-  #
+  DEFAULT_EMAIL = 'eaorigami@gmail.com'
+
   def person_created
     @user = params[:user]
     @person = params[:person]
 
-    mail to: "eaorigami@gmail.com", subject: "Person created: #{@person.name} (#{@person.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Person created: #{@person.name} (#{@person.id}) by #{@user.name}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.family_mailer.person_updated.subject
-  #
   def person_updated
     @user = params[:user]
     @person = params[:person]
 
-    mail to: "eaorigami@gmail.com", subject: "Person updated: #{@person.name} (#{@person.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Person updated: #{@person.name} (#{@person.id}) by #{@user.name}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.family_mailer.couple_created.subject
-  #
   def couple_created
     @user = params[:user]
     @couple = params[:couple]
     @person_1_name = Person.find(@couple.person1_id).name
     @person_2_name = Person.find(@couple.person2_id).name
 
-    mail to: "eaorigami@gmail.com", subject: "Couple created: #{@person_1_name} & #{@person_2_name} (#{@couple.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Couple created: #{@person_1_name} & #{@person_2_name} (#{@couple.id}) by #{@user.name}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.family_mailer.couple_updated.subject
-  #
   def couple_updated
     @user = params[:user]
     @couple = params[:couple]
     @person_1_name = Person.find(@couple.person1_id).name
     @person_2_name = Person.find(@couple.person2_id).name
 
-    mail to: "eaorigami@gmail.com", subject: "Couple updated: #{@person_1_name} & #{@person_2_name} (#{@couple.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Couple updated: #{@person_1_name} & #{@person_2_name} (#{@couple.id}) by #{@user.name}"
   end
 
   def couple_deleted
@@ -62,14 +47,9 @@ class FamilyMailer < ApplicationMailer
     @separation = params[:separation]
     @local = params[:local]
 
-    mail to: "eaorigami@gmail.com", subject: "Couple deleted: #{@person_1_name} & #{@person_2_name} (#{@id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Couple deleted: #{@person_1_name} & #{@person_2_name} (#{@id}) by #{@user.name}"
   end
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.family_mailer.child_created.subject
-  #
   def child_created
     @user = params[:user]
     @child = params[:child]
@@ -77,7 +57,7 @@ class FamilyMailer < ApplicationMailer
     @person_1_name = Person.find(@couple.person1_id).name
     @person_2_name = Person.find(@couple.person2_id).name
 
-    mail to: "eaorigami@gmail.com", subject: "Child created: #{@child.name} (#{@child.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Child created: #{@child.name} (#{@child.id}) by #{@user.name}"
   end
 
   def child_deleted
@@ -87,6 +67,6 @@ class FamilyMailer < ApplicationMailer
     @person_1_name = Person.find(@couple.person1_id).name
     @person_2_name = Person.find(@couple.person2_id).name
 
-    mail to: "eaorigami@gmail.com", subject: "Child deleted: #{@child.name} (#{@child.id}) by #{@user.name}"
+    mail to: DEFAULT_EMAIL, subject: "Child deleted: #{@child.name} (#{@child.id}) by #{@user.name}"
   end
 end
