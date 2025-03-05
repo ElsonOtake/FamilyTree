7310c1c4fcde1e474048714718dca0039d0d3a54
Elson Otake <elsonotake@yahoo.com.br>
2025-03-05 10:45:51 -0300

    Move deleted_at creation to previous migration

--- db/migrate/20230604004851_add_columns_to_user.rb
+++ db/migrate/20230604004851_add_columns_to_user.rb
@@ -2,5 +2,7 @@ class AddColumnsToUser < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :name, :string
     add_column :users, :phone, :string
+    add_column :users, :deleted_at, :datetime
+    add_index :users, :deleted_at
   end
 end
