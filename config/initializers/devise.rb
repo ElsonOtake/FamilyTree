0c37143f41dc4639ced8c7fcf8d7c04ba96b14da
Elson Otake <elsonotake@yahoo.com.br>
2025-04-21 21:07:54 -0300

    ajustes para deploy no Fly.io

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,11 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, ENV.fetch('GOOGLE_ID'), ENV.fetch('GOOGLE_SECRET'), {}
+  if ENV['GOOGLE_ID'] && ENV['GOOGLE_SECRET']
+    config.omniauth :google_oauth2, ENV.fetch('GOOGLE_ID'), ENV.fetch('GOOGLE_SECRET'), {}
+  else
+    Rails.logger.warn('GOOGLE_ID or GOOGLE_SECRET is missing. Google auth will not be configured.')
+  end
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
