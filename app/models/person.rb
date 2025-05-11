974b3a007fff655a06e4ab1ea5f479503e26c446
Elson Otake <47673311+ElsonOtake@users.noreply.github.com>
2025-05-10 21:58:53 -0300

    Update dates data display

--- app/models/person.rb
+++ app/models/person.rb
@@ -71,6 +71,30 @@ class Person < ApplicationRecord
     format_partial_date(death_day, death_month, death_year)
   end
 
+  def date_text
+    if death_year
+      if birth_year
+        "#{I18n.t('people.person.passed_away_on_years_of_age', date: formatted_long_date(death_year, death_month, death_day), age: formatted_age(birth_year, birth_month, birth_day, death_year, death_month, death_day))}"
+      else
+        "#{I18n.t('people.person.passed_away_on', date: formatted_long_date(death_year, death_month, death_day))}"
+      end
+    elsif birth_year && alive?
+      formatted_age(birth_year, birth_month, birth_day, Date.today.year, Date.today.month, Date.today.day)
+    end
+  end
+
+  def birth_text
+    if birth_year && birth_month && birth_day
+      "#{I18n.t('people.birthday')}: #{I18n.l(Date.new(birth_year, birth_month, birth_day), format: :long)}"
+    elsif birth_year && birth_month
+      "#{I18n.t('people.birthday')}: #{I18n.l(Date.new(birth_year, birth_month, Date.today.day), format: :month_year_long)}"
+    elsif birth_year
+      "#{I18n.t('people.birthday')}: #{I18n.l(Date.new(birth_year, Date.today.month, Date.today.day), format: :year)}"
+    elsif birth_month && birth_day
+      "#{I18n.t('people.birthday')}: #{I18n.l(Date.new(Date.today.year, birth_month, birth_day), format: :day_month_long)}"
+    end
+  end
+
   def slug_candidates
     [
       :name,
@@ -83,7 +107,7 @@ class Person < ApplicationRecord
   end
 
   def self.ransackable_attributes(_auth_object = nil)
-    %w[alive birth death description gender name kanji]
+    %w[alive birth birth_year birth_month birth_day death description gender name kanji]
   end
 
   def self.ransackable_associations(_auth_object = nil)
@@ -107,6 +131,42 @@ class Person < ApplicationRecord
       I18n.l(Date.new(year, Date.today.month, Date.today.day), format: :year)
     else
       ''
+    end
+  end
+
+  def formatted_long_date(year, month, day)
+    if year && month && day
+      I18n.l(Date.new(year, month, day), format: :long)
+    elsif month && year
+      I18n.l(Date.new(year, month, Date.today.day), format: :month_year_long)
+    elsif day && month
+      I18n.l(Date.new(Date.today.year, month, day), format: :day_month_long)
+    elsif year
+      I18n.l(Date.new(year, Date.today.month, Date.today.day), format: :year)
+    end
+  end
+
+  def formatted_age(from_year, from_month, from_day, to_year, to_month, to_day)
+    from_date = Date.new(from_year, from_month || 1, from_day || 1)
+    to_date = Date.new(to_year, to_month || 1, to_day || 1)
+    from_date, to_date = [from_date, to_date].sort
+    years = to_date.year - from_date.year
+    months = to_date.month - from_date.month
+    days = to_date.day - from_date.day
+    if days < 0
+      months -= 1
+      days += (to_date - 1.month).end_of_month.day
+    end
+    if months < 0
+      years -= 1
+      months += 12
+    end
+    if years > 0
+      I18n.t('datetime.distance_in_words.x_years_old', count: years)
+    elsif months > 0
+      I18n.t('datetime.distance_in_words.x_months', count: months)
+    else
+      I18n.t('datetime.distance_in_words.x_days', count: days)
     end
   end
 end
